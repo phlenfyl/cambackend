16a36847e35b798a4a9955782e23d8e8f5558747
phlenfyl <phlenfytest28@gmail.com>
2023-10-11 18:25:17 +0100

    resetted the apis in the corsheader

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$ok6*buoc-06t2^p&3aff(ki84i-9(gji!73pwsf7!%44sjrfs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mfmadmin-izldar4i.b4a.run', '*']
 
 
 # Application definition
