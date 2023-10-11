7d3e442940491558e6c07aa38d698b1f2189df59
phlenfyl <phlenfytest28@gmail.com>
2023-10-11 18:32:30 +0100

    added custom middleware

--- core/settings.py
+++ core/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 # django-taggit django-ckeditor-5 django-ckeditor django-embed-video djangorestframework cloudinary django-cors-headers djangoql django-import-export
 
 MIDDLEWARE = [
+    'core.middleware.AdminCORSMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
